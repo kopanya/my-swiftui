ce4cc416126f56f686611dc49e819b7f218cd416
Yao Yuan <yaoyuan@weipaitang.com>
2021-07-26 15:05:07 +0800

    Initial Commit

---
+++ my-swiftui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  my-swiftui
+//
+//  Created by yaoyuan on 2021/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ my-swiftui/my_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_swiftuiApp.swift
+//  my-swiftui
+//
+//  Created by yaoyuan on 2021/7/26.
+//
+
+import SwiftUI
+
+@main
+struct my_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
